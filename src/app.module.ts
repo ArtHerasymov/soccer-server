c26ea37de9c8e42a9f5053ee41fe457e1a3814c7
herasymova <hoyejab@be-breathtaking.net>
2020-10-07 01:13:50 +0300

    Docker configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TeamsModule } from './modules/teams/teams.module';
 import { MatchesModule } from './modules/matches/matches.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TeamsModule, MongooseModule.forRoot(process.env.MONGO_CONNECTION_URL), MatchesModule],
+  imports: [ConfigModule.forRoot(), TeamsModule, MongooseModule.forRoot(process.env.MONGO_CONNECTION_URL), MatchesModule],
   controllers: [],
   providers: [],
 })
